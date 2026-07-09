bc2a03e12cbdec30c089235b312be91181da70aa
Geovanni Pacheco <geovanni.m.pacheco@gmail.com>
2026-07-09 02:52:31 -0300

    Add resize handlers

--- gui-app/src/layouts/AppLayout.tsx
+++ gui-app/src/layouts/AppLayout.tsx
@@ -1,16 +1,17 @@
 import { ToastProvider } from "@/components/ui/toast";
 import { Outlet } from "react-router-dom";
 import { AppHeader } from "../components/AppHeader";
+import { WindowResizeBorder } from "../components/WindowResizeBorder";
 import { EngineProvider, useEngine } from "../hooks/useEngine";
 
 function AppLayoutContent() {
   const { status } = useEngine();
 
   return (
-    <div className="flex h-screen flex-col overflow-hidden bg-zinc-950 text-zinc-100">
+    <WindowResizeBorder className="flex flex-col bg-zinc-950 text-zinc-100">
       <AppHeader status={status} />
       <Outlet />
-    </div>
+    </WindowResizeBorder>
   );
 }
 
